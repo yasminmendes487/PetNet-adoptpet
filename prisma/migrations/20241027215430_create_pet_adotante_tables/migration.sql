6e103c5aef406383102d84157355b70fc7dc89b3
yasmin <yasminmendes487@gmail.com>
2024-10-27 20:21:35 -0300

    feat: configure environment, install Prisma, and connect to database

---
+++ prisma/migrations/20241027215430_create_pet_adotante_tables/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "adocoes" (
+    "id" SERIAL NOT NULL,
+    "pet_id" INTEGER,
+    "adotante_id" INTEGER,
+    "data_adocao" DATE NOT NULL,
+
+    CONSTRAINT "adocoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "adotantes" (
+    "id" SERIAL NOT NULL,
+    "nome" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "telefone" VARCHAR(50),
+    "endereco" TEXT,
+
+    CONSTRAINT "adotantes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" SERIAL NOT NULL,
+    "nome" VARCHAR(255) NOT NULL,
+    "especie" VARCHAR(255) NOT NULL,
+    "idade" INTEGER NOT NULL,
+    "descricao" TEXT,
+    "status" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "adotantes_email_key" ON "adotantes"("email");
+
+-- AddForeignKey
+ALTER TABLE "adocoes" ADD CONSTRAINT "adocoes_adotante_id_fkey" FOREIGN KEY ("adotante_id") REFERENCES "adotantes"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "adocoes" ADD CONSTRAINT "adocoes_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
