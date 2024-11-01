5adb0b769982f7f04da75c890dcb03a73600accd
Ingryd <90623907+ingrydf12@users.noreply.github.com>
2024-11-01 07:57:04 -0300

    fix git: update AdotanteRoutes.js

--- src/routes/AdotanteRoutes.js
+++ src/routes/AdotanteRoutes.js
@@ -4,8 +4,11 @@ import AdotanteController from "../controllers/AdotanteController";
 const adotanteRoutes = Router();
 const adotantesController = new AdotanteController();
 
+// TODO: Criar Métodos 
 adotanteRoutes.post('/', adotantesController.createAdotante);
 adotanteRoutes.get('/', adotantesController.listAdotantes);
-adotanteRoutes.get('/:id', adotantesController.FindByNameAdotante); 
+adotanteRoutes.get('/:name', adotantesController.FindByNameAdotante);
+adotanteRoutes.put('/:id', adotantesController.updateAdotante);
+adotanteRoutes.delete('/:id', adotantesController.removeAdotante);
 
 export { RoutesAdotantes }
