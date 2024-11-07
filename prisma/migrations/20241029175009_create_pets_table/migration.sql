eb28f54c88c22e1f255297c9383fc51bae92a6c4
luc0liv <luoliveiradacunha@gmail.com>
2024-11-06 23:09:25 -0300

    fix: remove arquivo sem uso e atualiza migrations

--- prisma/migrations/20241029175009_create_pets_table/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `data_nascimento` on table `pets` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "pets" ALTER COLUMN "data_nascimento" SET NOT NULL,
-ALTER TABLE "pets" ALTER COLUMN "data_nascimento" TYPE TIMESTAMP;
