d987de514f1f0310f0df696e656cdc118d7461b2
luc0liv <luoliveiradacunha@gmail.com>
2024-11-06 23:08:53 -0300

    feat: cria rota para adoção

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@
 import { Router } from "express";
 import { petsRoutes } from "./PetRoutes.js";
 import { adotantesRoutes } from "./AdotantesRoutes.js";
+import { adocaoRoutes } from "./AdocaoRoutes.js";
 
 const router = Router();
 router.use('/pets', petsRoutes);
 router.use('/adotantes', adotantesRoutes);
+router.use('/adocao', adocaoRoutes);
 
 export { router }
